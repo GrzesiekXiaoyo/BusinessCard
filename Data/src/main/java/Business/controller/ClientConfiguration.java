01e88a966f71df76b5d346f62d85294654611b1d
Xiaoyo <grzesiekk15@gmail.com>
2018-01-30 22:11:52 +0100

    Changes in main

--- Data/src/main/java/Business/controller/ClientConfiguration.java
+++ Data/src/main/java/Business/controller/ClientConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/ClientConfiguration/")
+@RequestMapping(value = "api/v1/ClientConfiguration/")
 public class ClientConfiguration
 {
 	@GetMapping
