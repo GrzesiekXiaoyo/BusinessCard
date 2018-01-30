e64dbc10e8deeac4a7282f9d31d51b5289cd8aeb
Xiaoyo <grzesiekk15@gmail.com>
2018-01-30 20:46:29 +0100

    Easy controller which said hello

---
+++ Data/src/main/java/Business/controller/ClientConfiguration.java
@@ -0,0 +1,16 @@
+package Business.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/ClientConfiguration/")
+public class ClientConfiguration
+{
+	@GetMapping
+	public String GetClientConfiguration()
+	{
+		return "Hello";
+	}
+}
