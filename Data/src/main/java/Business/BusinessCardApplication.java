a997a81199f3ff7363d6693e9dabd5c6dc9d34fb
Xiaoyo <grzesiekk15@gmail.com>
2018-01-31 22:33:40 +0100

    Changes in pom

--- Data/src/main/java/Business/BusinessCardApplication.java
+++ Data/src/main/java/Business/BusinessCardApplication.java
@@ -1,12 +1,18 @@
 package Business;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class BusinessCardApplication extends SpringBootServletInitializer
 {
